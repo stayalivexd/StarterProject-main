de3cce2c5ac67872f8b9b1bd20b8e706482f1541
Lauri Leppanen <leppnenlauri@gmail.com>
2024-05-02 16:04:39 +0200

    Initial commit

---
+++ Assets/Prefabs/EyetrackingRay.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+
+public class EyetrackingRay : MonoBehaviour
+{
+    [SerializeField]
+    private float rayDistance = 1.0f;
+    [SerializeField]
+    private float rayWidth = 0.01f;
+    [SerializeField]
+    private LayerMask layersToIncude;
+    [SerializeField]
+    private Color rayColorDefaultState = Color.white;
+    [SerializeField]
+    private Color rayColorHoverState = Color.red;
+
+    private LineRenderer lineRenderer;
+    private List<EyeInteractable> eyeInteractables = new List<EyeInteractable>();
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        SetupRay();
+    }
+
+    void SetupRay()
+    {
+        lineRenderer.useWorldSpace = false;
+        lineRenderer.positionCount = 2;
+        lineRenderer.startWidth = rayWidth;
+        lineRenderer.startColor = rayColorDefaultState;
+        lineRenderer.endColor = rayColorHoverState;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, new Vector3(transform.position.x, transform.position.y, transform.position.z +rayDistance));
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        Vector3 rayCastDirection = transform.TransformDirection(Vector3.forward) * rayDistance;
+
+        if (Physics.Raycast(transform.position, rayCastDirection, out hit, Mathf.Infinity, layersToIncude))
+        {
+            UnSelect();
+            lineRenderer.startColor = rayColorHoverState;
+            lineRenderer.endColor = rayColorHoverState;
+            var eyeInteractable = hit.transform.GetComponent<EyeInteractable>();
+            eyeInteractables.Add(eyeInteractable);
+            eyeInteractable.IsHovered = true;
+            UnSelect(true);
+        }
+    }
+
+    void UnSelect(bool clear = false)
+    {
+        foreach (var interactable in eyeInteractables)
+        {
+            interactable.IsHovered = false;
+        }
+        if (clear)
+        {
+            eyeInteractables.Clear();
+        }
+    }
+
+
+}
