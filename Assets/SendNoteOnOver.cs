f886d6ef09727caabc016957d2ebfb379f8c67b0
ieie <mintbutter@protonmail.com>
2024-06-05 10:05:23 +0200

    Video Texture Play On Note

--- Assets/SendNoteOnOver.cs
+++ Assets/SendNoteOnOver.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 namespace extOSC.Examples
 {
@@ -8,12 +9,12 @@ namespace extOSC.Examples
     {
 
         public bool OnMouseEnterActive = false;
-
+        public GameObject bigCube;
         public int pitch;
         public int velocity;
 
         public string SendAddress = "/hand0";
-       
+
 
         public string ReceiveNote = "/Note1";
         public string ReceiveVelocity = "/Velocity1";
@@ -25,6 +26,12 @@ namespace extOSC.Examples
         public int LastReceivedVelocity = 0;
         public Renderer IdleObject;
         public Renderer GuideObject;
+
+        public VideoClip videoClip;
+        public VideoPlayer videoPlayer;
+        public Renderer targetRenderer;
+        
+
 
         protected virtual void Start()
         {
@@ -51,7 +58,13 @@ namespace extOSC.Examples
             if (!OnMouseEnterActive) return;
 
             SendMidiNote(pitch, velocity);
-            changeColorTo(Color.red);
+            //changeColorTo(Color.red);
+            //videoPlayer = bigCube.AddComponent<VideoPlayer>();
+
+            videoPlayer.renderMode = VideoRenderMode.MaterialOverride;
+
+
+            videoPlayer.Play();
 
         }
 
@@ -81,23 +94,46 @@ namespace extOSC.Examples
             Transmitter.Send(message);
         }
 
-        private void changeColorTo (Color toColor)
+        private void changeColorTo(Color toColor)
         {
             // Get the Renderer component from the new cube
+
             var cubeRenderer = GetComponent<Renderer>();
+            var bigCubeRenderer = bigCube.GetComponent<Renderer>();
+           
+
 
             // Call SetColor using the shader property name "_Color" and setting the color to red
             cubeRenderer.material.SetColor("_Color", toColor);
+            bigCubeRenderer.material.SetColor("_Color", toColor);
         }
 
-        private void ChangeIdleObjectColorTo (Color toColor)
+        private void ChangeIdleObjectColorTo(Color toColor)
         {
             IdleObject.material.SetColor("_Color", toColor);
+
+
         }
 
         private void ChangeGuideObjectColorTo(Color toColor)
         {
             GuideObject.material.SetColor("_Color", toColor);
+
+
+
+        }
+        private void ChangeMainCubeColor(Color toColor)
+
+        {
+            targetRenderer.material.SetColor("_Color", toColor);
+        }
+
+        private void StartGuideAnim()
+        {
+            //videoPlayer = gameObject.AddComponent<VideoPlayer>();
+            //videoPlayer.playOnAwake = false; 
+            //videoPlayer.renderMode = VideoRenderMode.MaterialOverride;
+           
         }
 
         //Receiving messages code
@@ -115,13 +151,23 @@ namespace extOSC.Examples
                     if (LastReceivedVelocity > 0)
                     {
                         ChangeGuideObjectColorTo(Color.green);
+                        videoPlayer = bigCube.GetComponent<VideoPlayer>();
+                        videoPlayer.Play();
+
+
+                        //bigCube.GetComponent<VideoPlayer>();
+                        //bigCube.GetComponent<Renderer>().material.mainTexture.Play();
+                        //movie.Play();
                         //changeColorTo(Color.green);
                         //ChangeIdleObjectColorTo(Color.gray);
                     }
                     else
                     {
+                        videoPlayer.Stop();
                         //changeColorTo(Color.gray);
                         ChangeGuideObjectColorTo(Color.gray);
+                       // ChangeMainCubeColor(Color.white);
+                       //movie.Pause();
                         //ChangeIdleObjectColorTo(Color.yellow);
                     }
                 }
