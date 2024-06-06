ff77ccbc2f4ee76db6454035f07c82547967de4e
stayalivexd <leppnenlauri@gmail.com>
2024-06-06 13:45:11 +0200

    Guideball adjustments

--- Assets/Prefabs/TestGuideBall.cs
+++ Assets/Prefabs/TestGuideBall.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class TestGuideBall : MonoBehaviour
 {
     public GuideBall guideBall; // Reference to the GuideBall script
+    private bool isMoving = false;
 
     void Update()
     {
-        // Move the GuideBall to a random position when the space key is pressed
+        // Start moving the GuideBall when the space key is pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            int randomIndex = Random.Range(0, guideBall.positions.Length);
-            guideBall.MoveToPosition(randomIndex);
+            isMoving = true;
+            StartCoroutine(MoveGuideBall());
+        }
+    }
+
+    IEnumerator MoveGuideBall()
+    {
+        int currentIndex = 0;
+
+        while (isMoving)
+        {
+            guideBall.MoveToPosition(currentIndex);
+
+            // Wait for some time before moving to the next position
+            yield return new WaitForSeconds(2.0f);
+
+            // Move to the next position in the array
+            currentIndex = (currentIndex + 1) % guideBall.positions.Length;
         }
     }
 }
