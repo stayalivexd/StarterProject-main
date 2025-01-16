f9ac79e0cda8906ff1cf25d7b6dca65569acc4bd
ieiepochehli <mintbutter@protonmail.com>
2025-01-16 09:41:34 +0100

    guitar sounds

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class PauseMenu : MonoBehaviour
+//{
+//    public KeyCode key;
+//    private object pause;
+
+
+//    void Start()
+//    {
+//        pause = GetComponent<object>();
+//    }
+
+
+//    void Update()
+//    {
+//        if (Input.GetKeyUp(KeyCode.Space))
+//        {
+//            pause.GetComponent = active;
+//        }
+//    }
+
